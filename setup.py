38363b74c03bd688e0e3d89d1176cc21059f802b
Nelson-iitp <nelson_2121cs07@iitp.ac.in>
2022-05-30 18:56:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name =          'known',
-    version =       '0.0.3',  # 0.0.x is for unstable versions
+    version =       '0.0.4',  # 0.0.x is for unstable versions
     url =           "https://github.com/NelsonSharma/Public",
     author =        "Nelson.S",
     author_email =  "mail.nelsonsharma@gmail.com",
@@ -16,10 +16,3 @@ setup(
     install_requires = [],
     include_package_data=True
 )
-
-# cd ..../src
-# python38 -m build
-# python38 -m twine upload dist/*
-# NelsonSharma
-
-# python38 -m pip install -e ./
